778186b4fdcd9afa1072c2333a726de857c2fe62
pdabrowski <przemyslaw.dabrowski95@gmail.com>
2022-05-21 14:59:31 +0200

    initial commit

---
+++ src/main/java/pl/dabrowski/electrotools/ElectroToolsApplication.java
@@ -0,0 +1,13 @@
+package pl.dabrowski.electrotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dabrowski/electrotools/ElectroToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dabrowski.electrotools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
