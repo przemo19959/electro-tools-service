d0115b123a0194b04a0d416434b4aad32f7246a9
pdabrowski <przemyslaw.dabrowski95@gmail.com>
2022-05-21 18:35:07 +0200

    front fixed after migration, querydsl added

--- src/main/java/pl/dabrowski/electrotools/ElectroToolsApplication.java
+++ src/main/java/pl/dabrowski/electrotools/ElectroToolsApplication.java
@@ -2,8 +2,12 @@ package pl.dabrowski.electrotools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class ElectroToolsApplication {
 
     public static void main(String[] args) {
